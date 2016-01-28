0e5d9062147e2355a1a4b61ff74a6bd6af1b6b10
ingktds <tadashi.1027@gmail.com>
2016-01-28 11:00:22 +0900

    Access-Control-Allow-Originヘッダ追加 Toban::Model::Tobanの呼び出し追加

--- lib/Toban/Controller/Toban.pm
+++ lib/Toban/Controller/Toban.pm
@@ -1,11 +1,14 @@
 package Toban::Controller::Toban;
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::JSON;
-use Toban::Model::Member;
+use Toban::Model::Toban;
 
 sub list {
     my $c = shift;
-    my $member = Toban::Model::Member->member_list;
+
+    $c->res->headers->header( 'Access-Control-Allow-Origin' => '*' );
+    my $member = Toban::Model::Toban->list;
+
     $c->render(json => $member);
     
 }
