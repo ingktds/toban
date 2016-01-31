71f8856a67542a250783fa847e601c020823add4
ingktds <tadashi.1027@gmail.com>
2016-02-01 05:34:00 +0900

    Add update method Add a property for eventOrder

--- lib/Toban/Model/Toban.pm
+++ lib/Toban/Model/Toban.pm
@@ -38,8 +38,28 @@ sub _get_calendar {
         $self->push_toban({
                 start => $doc->{start},
                 title => $doc->{title},
+                order => $doc->{id},
         });
     }
+}
+
+sub update {
+    my ( $self, $args ) = @_;
+    my $start = $args->{start};
+    my $title = $args->{title};
+
+    my $mongo = Toban::DB::Mongo->new( collection => 'Toban');
+    my $collection = $mongo->get_collection();
+    my $calendars = $collection->find;
+
+    $calendars->update_one({
+        { "start" => $start },
+        { '$set' =>
+            {
+                "title" => $title,
+            }
+        }
+    });
 }
 
 1;
