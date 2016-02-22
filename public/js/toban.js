6d9af2c55879ba2b299686f706d6076c0f924198
ingktds <tadashi.1027@gmail.com>
2016-02-22 21:14:25 +0900

    Add update function

--- public/js/toban.js
+++ public/js/toban.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
-  $('#calendar').fullCalendar({
-    firstDay: 1,
-    eventSources: [
-      {
-        url: 'http://api.toban.ingk.xyz/v1/assignment',
-        editable: true
-      },
-      {
-        url: 'http://api.toban.ingk.xyz/v1/calendar',
-        color: '#3CB371',
-        editable: false
-      }
-    ],
+    $('#calendar').fullCalendar({
+        firstDay: 1,
+        eventSources: [
+            {
+                url: 'http://api.toban.ingk.xyz/v1/assignment',
+                editable: true
+            },
+            {
+                url: 'http://api.toban.ingk.xyz/v1/calendar',
+                color: '#3CB371',
+                editable: false
+            }
+        ],
     eventOrder: 'order',
     eventDrop: function(event) {
+        //console.dir(event);
+
+        // Drop先の日付を取得
+        var drop_date = $.fullCalendar.moment(event._start._d);
+        var date = drop_date.format('YYYY-MM-DD');
+
         $.ajax({
             type: 'PUT',
             url: 'http://api.toban.ingk.xyz/v1/assignment',
             crossDamain: true,
+            dataType: 'json',
             data: {
                 member: event.title,
-                date: "2016-02-03"
+                date: date,
+                id: event.order
             }
         });
+
     }
   })
 
