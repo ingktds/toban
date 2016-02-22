97127a61f629582a1575bcc5d3849dfa4bba27b9
ingktds <tadashi.1027@gmail.com>
2016-02-22 21:19:36 +0900

    Fix a bug that can't update data with CORS

--- lib/Toban/Controller/Toban.pm
+++ lib/Toban/Controller/Toban.pm
@@ -16,7 +16,7 @@ sub list {
 sub update {
     my $c = shift;
 
-    $c->res->headers->header( 'Access-Control-Allow-Origin' => $c->app->config->{mojo}{www_url} );
+    $c->res->headers->header( 'Access-Control-Allow-Origin' => 'http://' . $c->app->config->{mojo}{host}{www} );
     $c->res->headers->header( 'Access-Control-Allow-Credentials' => 'true' );
     $c->res->headers->header( 'Access-Control-Allow-Methods' => 'PUT, OPTIONS' );
     $c->res->headers->header( 'Access-Control-Allow-Headers' => 'Accept, Content-Type' );
